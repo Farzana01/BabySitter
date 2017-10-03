7905ce98e76ab28cb94b809a49000bc6808d66b1
Farzana01 <farzana.n.azad@gmail.com>
2017-10-03 13:18:48 -0400

    passed 2 test

--- src/main/java/BabySitterPayCalculator.java
+++ src/main/java/BabySitterPayCalculator.java
@@ -2,7 +2,7 @@
 public class BabySitterPayCalculator {
 
 	private int bedTime = 20;
-	
+
 	public int getBedTime() {
 		return bedTime;
 	}
@@ -16,11 +16,25 @@ public class BabySitterPayCalculator {
 	}
 
 	public boolean validEndTime(int currentTime) {
-			if ((currentTime >=0 && currentTime <= 4)) {
-				return true;
-			} return false;
-			
+		if ((currentTime >= 0 && currentTime <= 4)) {
+			return true;
+		}
+		return false;
+
 	}
-      
-	
+
+	public int getPaid(int startTime, int endTime) {
+		int totalPay = 0;
+		int hourlyPay = 0;
+		for (int currentTime = startTime; currentTime < endTime
+				|| (endTime > 0 && endTime <= 4 && (currentTime - 24) < endTime); currentTime++) {
+			if (currentTime < bedTime) {
+				hourlyPay = 12;
+
+			}
+			
+		}
+		totalPay +=hourlyPay;
+		return totalPay;
+	}
 }
--- src/test/java/BabySitterPayCalculatorTest.java
+++ src/test/java/BabySitterPayCalculatorTest.java
@@ -4,44 +4,45 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class BabySitterPayCalculatorTest {
-	
-	
+
 	BabySitterPayCalculator babySitterPayCalculator;
-	
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		babySitterPayCalculator = new BabySitterPayCalculator();
 	}
-	
+
 	@Test
-	 public void shouldBeStartTimeBefore5pmIsNotValid() {
-				
+	public void shouldBeStartTimeBefore5pmIsNotValid() {
+
 		int startTime = 16;
 		assertFalse(babySitterPayCalculator.validStartTime(startTime));
 	}
-	
+
 	@Test
-	 public void shouldBeStartTimeAfter5pmIsNotValid() {
-				
+	public void shouldBeStartTimeAfter5pmIsNotValid() {
+
 		int startTime = 18;
 		assertTrue(babySitterPayCalculator.validStartTime(startTime));
 	}
-	
+
 	@Test
-	 public void shouldBeEndTimeAfter4AmIsNotValid() {
-				
+	public void shouldBeEndTimeAfter4AmIsNotValid() {
+
 		int endTime = 5;
 		assertFalse(babySitterPayCalculator.validStartTime(endTime));
 	}
-	
+
 	@Test
-	 public void shouldBeBedTimeAt8Pm() {
-						
-		assertEquals(20,babySitterPayCalculator.getBedTime());
+	public void shouldBeBedTimeAt8Pm() {
+
+		assertEquals(20, babySitterPayCalculator.getBedTime());
 	}
 	
-	
-	
-	
-	
+	@Test
+	 public void shouldPay12DollarsBetween5pmAnd8pm() {
+						
+		assertEquals(12,babySitterPayCalculator.getPaid(17,20));
+
+}
 }
